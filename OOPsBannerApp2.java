fadd05dbbf915eb59fd29d2fe8cf78f3b9336871
pankhurimishra48-art <pm8785@srmist.edu.in>
2026-02-24 15:06:04 +0530

    Feature(UC1):PrintOOPSBannerjoin

--- OOPsBannerApp2.java
+++ OOPsBannerApp2.java
@@ -1,5 +1,13 @@
-public class OOPsBannerApp2{
-public static void main(string[] args){
-system.out.println9("welcome");
-}
+public class PrintOOPSBannerjoin {
+    public static void main(String[] args) {
+
+        System.out.println(String.join(" ", "   ***   ", "   ***   ", " ******  ", "  *****  "));
+        System.out.println(String.join(" ", " **   ** ", " **   ** ", " **    ** ", " **    ** "));
+        System.out.println(String.join(" ", "**     **", "**     **", " **     **", "**       "));
+        System.out.println(String.join(" ", "**     **", "**     **", " **    ** ", "  ****   "));
+        System.out.println(String.join(" ", "**     **", "**     **", " ******   ", "      ** "));
+        System.out.println(String.join(" ", " **   ** ", " **   ** ", " **       ", "**     **"));
+        System.out.println(String.join(" ", "   ***   ", "   ***   ", " **       ", "  *****  "));
+        
+    }
 }
